41be1a15d39f018b8ba90eac5dfb57d4c47728b0
Luciaon Aibar <lucianoaibar@gmail.com>
2020-07-22 22:42:19 -0300

    First commit

---
+++ Nodo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindPath {
+	public class Nodo {
+
+		//_____________________________________________________________________
+		public string		Palabra;
+		public List<string>	Relacionadas;
+		public Nodo			Padre;
+		public int			indice_actual;
+
+		//_____________________________________________________________________
+		public Nodo(string palabra, List<string> diccionario, Nodo padre) {
+
+			char[]	palabra_chars;
+			char[]	chars;
+			int		dif;
+			int		len, n;
+
+			this.Palabra		= palabra;
+			this.Relacionadas	= new List<string>();
+			this.Padre			= padre;
+			this.indice_actual	= -1;
+
+			palabra_chars = palabra.ToCharArray();
+			len = palabra.Length;
+
+			foreach(string s in diccionario) {
+				chars = s.ToCharArray();
+				dif = 0;
+				for(n=0; n<len; n++) {
+					if(chars[n]!=palabra_chars[n]) dif++;
+				}
+				if(dif==1) {
+					this.Relacionadas.Add(s);
+				}
+			}
+		}
+
+		//_____________________________________________________________________
+		public string DameRelacionada() {
+			if(this.indice_actual==this.Relacionadas.Count()-1) {
+				this.indice_actual = -1;
+				return null;
+			}
+			this.indice_actual++;
+			return this.Relacionadas[this.indice_actual];
+		}
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindPath {
+	class Program {
+		static void Main(string[] args) {
+
+			List<string> S = new List<string>() {
+				"rampa", "campa", "mamta", "cloro", "razpa", "razoa", "razon"
+				//"papa","paca", "caca","cama","mama"
+			};
+
+			string p1 = "rampa";
+			string p2 = "razon";
+			//string p1 = "papa";
+			//string p2 = "mama";
+
+			//S.Reverse();
+
+			List<Nodo>		Nodos = new List<Nodo>();
+			Nodo			nodo_padre;
+			Nodo			nodo;
+			string			palabra;
+
+			nodo_padre	= null;
+			palabra		= p1;
+			nodo		= null;
+			
+			for(;;){
+				if(palabra!=null) {
+					S.Remove(palabra);
+					nodo = new Nodo(palabra, S, nodo_padre);
+					Console.WriteLine(palabra);
+					if(nodo.Relacionadas.Contains(p2)) break;
+				}
+			
+				palabra = nodo.DameRelacionada();
+				if(palabra==null) {
+					nodo = nodo.Padre;
+					if(nodo==null) break;
+					S.Add(nodo.Palabra);
+					Console.WriteLine("<-");
+				}
+
+				nodo_padre = nodo;
+			}
+
+			Console.WriteLine(p2);
+		}
+
+	}
+}
